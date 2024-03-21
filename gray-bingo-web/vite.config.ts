4d5e1a30af084882ed5b147d061cfaaff6a5c711
Lpj+@dy204 <pengjunlee@163.com>
2024-03-21 13:50:33 +0800

    web

--- gray-bingo-web/vite.config.ts
+++ gray-bingo-web/vite.config.ts
@@ -4,8 +4,6 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 // @ts-ignore
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
-// @ts-ignore
-import { chromeExtension } from "./build/chromeExtension";
 
 // https://vitejs.dev/config/
 export default defineConfig({
